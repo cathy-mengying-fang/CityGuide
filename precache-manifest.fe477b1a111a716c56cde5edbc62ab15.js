2540adbb010e3dfdb9b6054641783ab23415cee8
cathy-mengying-fang <fangcat16@icloud.com>
2018-10-31 10:36:50 -0400

    Updates

---
+++ precache-manifest.fe477b1a111a716c56cde5edbc62ab15.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "544ea2f21a6773ff6acd2b8ce2063bcb",
+    "url": "/CityGuide/static/media/yu_garden.544ea2f2.png"
+  },
+  {
+    "revision": "2acd3061abcd5c826b87",
+    "url": "/CityGuide/static/css/main.2b645088.chunk.css"
+  },
+  {
+    "revision": "7eaa00c46f473d64509c",
+    "url": "/CityGuide/static/js/1.7eaa00c4.chunk.js"
+  },
+  {
+    "revision": "a3be4b6b525e9d6001e0",
+    "url": "/CityGuide/static/js/runtime~main.a3be4b6b.js"
+  },
+  {
+    "revision": "3e233120f86ff81c061c31557acb53e9",
+    "url": "/CityGuide/static/media/lorem_info.3e233120.png"
+  },
+  {
+    "revision": "418c6290ad8b85d7439f5735a0be01cd",
+    "url": "/CityGuide/static/media/pearl_tower.418c6290.png"
+  },
+  {
+    "revision": "2acd3061abcd5c826b87",
+    "url": "/CityGuide/static/js/main.2acd3061.chunk.js"
+  },
+  {
+    "revision": "07699ba29a6c44778fc5892601315ea3",
+    "url": "/CityGuide/static/media/shikumen.07699ba2.png"
+  },
+  {
+    "revision": "5e0e10fb1b6206d71fba656911d4d9ba",
+    "url": "/CityGuide/static/media/maglev.5e0e10fb.png"
+  },
+  {
+    "revision": "2fd1bfa9d39f5338d2569be0262ca510",
+    "url": "/CityGuide/static/media/pearl_tower_idle.2fd1bfa9.png"
+  },
+  {
+    "revision": "4b777107b8228f44d05d675b8448af70",
+    "url": "/CityGuide/static/media/yu_garden_idle.4b777107.png"
+  },
+  {
+    "revision": "037b03bff14b8d92064493caf0f3a4a0",
+    "url": "/CityGuide/static/media/shikumen_idle.037b03bf.png"
+  },
+  {
+    "revision": "df34b9bd654d2149964298e1cc61e6e0",
+    "url": "/CityGuide/index.html"
+  }
+];
